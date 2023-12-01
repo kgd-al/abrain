b28e80946c5a44b7502933b3b0259596fbd5f217
Kevin Godin-Dubois <k.j.m.godin-dubois@vu.nl>
2023-12-01 07:16:37 +0100

    .

--- src/abrain/core/ann.py
+++ src/abrain/core/ann.py
@@ -1,9 +1,16 @@
+import logging
+import pprint
 from enum import Flag, auto
+from pathlib import Path
 from typing import Optional, Dict
 
+import pandas as pd
 import plotly.graph_objects as go
 
 from .._cpp.phenotype import ANN, Point
+
+
+logger = logging.getLogger(__name__)
 
 
 def plotly_render(ann: ANN, labels: Optional[Dict[Point, str]] = None) \
@@ -93,15 +100,137 @@ def plotly_render(ann: ANN, labels: Optional[Dict[Point, str]] = None) \
 
 
 class ANNMonitor:
-    class MonitorLevel(Flag):
-        INPUTS = auto()
-        HIDDEN = auto()
-        OUTPUTS = auto()
-        NONE = 0
-        ALL = INPUTS | HIDDEN | OUTPUTS
-
-    def __init__(self, ann: ANN, level: MonitorLevel):
+    def __init__(self, ann: ANN, labels: dict[Point, str],
+                 folder: Path,
+                 neurons_file: Optional[Path],
+                 dynamics_file: Optional[Path]):
         self.ann = ann
+        self.labels = labels
+
+        self.save_folder = folder
+        if neurons_file:
+            self.neural_data_file = folder.joinpath(neurons_file)
+
+        self.neurons_data = None
+        if neurons_file or dynamics_file:
+            self.neurons_data = pd.DataFrame(
+                columns=[f"{n.pos}:{self.labels.get(n.pos)}"
+                         for n in self.ann.neurons()]
+            )
+
+        self.axonal_data = None
+        if dynamics_file:
+            self.interactive_plot_file = folder.joinpath(dynamics_file)
+            self.axonal_data = pd.DataFrame(
+                columns=[f"{l_.src().pos}->{n.pos}"
+                         for n in self.ann.neurons() for l_ in n.links()]
+            )
+            # pprint.pprint(self.axonal_data.columns)
+
+    def open(self):
+        pass
 
     def step(self):
-        print("[kgd-debug] Storing data")
+        if self.neurons_data is not None:
+            self.neurons_data.loc[len(self.neurons_data)] = [
+                n.value for n in self.ann.neurons()
+            ]
+        if self.axonal_data is not None:
+            self.axonal_data.loc[len(self.axonal_data)] = [
+                l_.src().value * l_.weight
+                for n in self.ann.neurons() for l_ in n.links()
+            ]
+
+    def close(self):
+        if self.neural_data_file:
+            self.neurons_data.to_csv(self.neural_data_file, sep=' ')
+            logger.info(f"Generated {self.neural_data_file}")
+
+        if self.interactive_plot_file:
+            # print(self.axonal_data)
+
+            x, y, z = zip(*[n.pos.tuple() for n in self.ann.neurons()])
+            # fig = go.Figure(
+            #     data=[go.Scatter3d(x=x, y=y, z=z,
+            #                        mode="markers",
+            #                        marker=dict(color="red", size=10))]
+            # )
+
+            fig = plotly_render(self.ann, self.labels)
+
+            # print([row for row in self.neurons_data.iterrows()])
+            frames = [
+                go.Frame(
+                    data=[
+                        go.Scatter3d(
+                            x=x, y=y, z=z,
+                            mode='markers',
+                            marker=dict(
+                                size=12,
+                                color=row,
+                                colorscale='RdBu_r',
+                                colorbar=dict(
+                                    title="Colorbar"
+                                )
+                            )
+                        )
+                    ],
+                    name=f'frame{index}'
+                ) for index, row in self.neurons_data.iterrows()
+            ]
+            fig.update(frames=frames)
+
+            def frame_args(duration):
+                return {
+                    "frame": {"duration": duration},
+                    "mode": "immediate",
+                    "fromcurrent": True,
+                    "transition": {"duration": duration, "easing": "linear"},
+                }
+
+            sliders = [
+                {"pad": {"b": 10, "t": 60},
+                 "len": 0.9,
+                 "x": 0.1,
+                 "y": 0,
+
+                 "steps": [
+                     {"args": [[f.name], frame_args(0)],
+                      "label": str(k),
+                      "method": "animate",
+                      } for k, f in enumerate(fig.frames)
+                 ]
+                 }
+            ]
+
+            fig.update_layout(
+
+                updatemenus=[{"buttons": [
+                    {
+                        "args": [None, frame_args(50)],
+                        "label": "Play",
+                        "method": "animate",
+                    },
+                    {
+                        "args": [[None], frame_args(0)],
+                        "label": "Pause",
+                        "method": "animate",
+                    }],
+
+                    "direction": "left",
+                    "pad": {"r": 10, "t": 70},
+                    "type": "buttons",
+                    "x": 0.1,
+                    "y": 0,
+                }
+                ],
+                sliders=sliders
+            )
+
+            fig.write_html(self.interactive_plot_file,
+                           auto_play=False)
+            logger.info(f"Generated {self.interactive_plot_file}")
+            # fig.show()
+            exit(42)
+
+        # print("[kgd-debug] testing pyplot dynamical rendering")
